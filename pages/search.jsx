d2bd742d0b687422b296e0d5b4918c6fc56555ed
lameboredghini <msm19b031@iiitdm.ac.in>
2023-03-04 18:10:50 +0530

    added filter with errors

---
+++ pages/search.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Filter from '../components/filter'
+
+
+
+const search = () => {
+  return (
+    <div>
+        <Filter></Filter>
+    </div>
+  )
+}
+
+export default search
